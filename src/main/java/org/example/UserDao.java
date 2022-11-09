d3f03dee8ca76c316b7888c42b2eeaadbeff272f
hstar0124 <dunjun10@naver.com>
2022-11-09 16:30:44 +0900

    [UPDATE] JDBC TEMLPATE 리팩토링

--- src/main/java/org/example/UserDao.java
+++ src/main/java/org/example/UserDao.java
@@ -4,81 +4,75 @@ import java.sql.*;
 
 public class UserDao {
 
-    private Connection getConnection() {
-        String url = "jdbc:h2:mem://localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=-1";
-        String id = "sa";
-        String pw = "";
-
-        try {
-            Class.forName("org.h2.Driver");
-            return DriverManager.getConnection(url, id, pw);
-        } catch (Exception ex) {
-            return null;
-        }
-    }
 
     public void create(User user) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
+//        jdbcTemplate.excuteUpdate(user, sql, new PreparedStatementSetter() {
+//            @Override
+//            public void setter(PreparedStatement pstmt) throws SQLException {
+//                pstmt.setString(1, user.getUserId());
+//                pstmt.setString(2, user.getPassword());
+//                pstmt.setString(3, user.getName());
+//                pstmt.setString(4, user.getEmail());
+//            }
+//        });
 
-        try {
-            con = getConnection();
-            String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
-            pstmt = con.prepareStatement(sql);
+        jdbcTemplate.excuteUpdate(user, sql, pstmt -> {
             pstmt.setString(1, user.getUserId());
             pstmt.setString(2, user.getPassword());
             pstmt.setString(3, user.getName());
             pstmt.setString(4, user.getEmail());
-
-            pstmt.executeUpdate();
-        } finally {
-            if(pstmt != null)
-                pstmt.close();
-
-            if(con != null)
-                con.close();
-        }
-
-
-
+        });
     }
 
 
-
+    //    public User findByUserId(String userId) throws SQLException {
+//        Connection con = null;
+//        PreparedStatement pstmt = null;
+//        ResultSet rs = null;
+//
+//        try {
+//            con = ConnectionManager.getConnection();
+//            String sql = "SELECT userId, password, name, email, FROM USERS WHERE userid = ?";
+//            pstmt = con.prepareStatement(sql);
+//            pstmt.setString(1, userId);
+//
+//            rs = pstmt.executeQuery();
+//
+//            User user = null;
+//            if(rs.next()) {
+//                user = new User(
+//                        rs.getString("userId"),
+//                        rs.getString("password"),
+//                        rs.getString("name"),
+//                        rs.getString("email")
+//                );
+//            }
+//            return user;
+//        } finally {
+//            if(rs != null) {
+//                rs.close();
+//            }
+//            if(pstmt != null) {
+//                pstmt.close();
+//            }
+//            if(con != null) {
+//                con.close();
+//            }
+//        }
+//
+//    }
     public User findByUserId(String userId) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
 
-        try {
-            con = getConnection();
-            String sql = "SELECT userId, password, name, email, FROM USERS WHERE userid = ?";
-            pstmt = con.prepareStatement(sql);
-            pstmt.setString(1, userId);
-
-            rs = pstmt.executeQuery();
-
-            User user = null;
-            if(rs.next()) {
-                user = new User(
-                        rs.getString("userId"),
-                        rs.getString("password"),
-                        rs.getString("name"),
-                        rs.getString("email")
-                );
-            }
-            return user;
-        } finally {
-            if(rs != null) {
-                rs.close();
-            }
-            if(pstmt != null) {
-                pstmt.close();
-            }
-            if(con != null) {
-                con.close();
-            }
-        }
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        String sql = "SELECT userId, password, name, email, FROM USERS WHERE userid = ?";
+        return (User) jdbcTemplate.executeQuery(userId, sql,
+                pstmt -> pstmt.setString(1, userId),
+                resultSet -> new User(resultSet.getString("userId"),
+                        resultSet.getString("password"),
+                        resultSet.getString("name"),
+                        resultSet.getString("email")));
 
     }
 }
